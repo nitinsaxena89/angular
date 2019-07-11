65ea5ffa23661a0cefad20967b3bb27f7f4d1833
Nitin <nitin_saxena@live.in>
2019-07-11 16:21:31 +0530

    update

--- src/app/signuppage/signuppage.component.ts
+++ src/app/signuppage/signuppage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signuppage',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signuppage.component.css']
 })
 export class SignuppageComponent implements OnInit {
-
-  constructor() { }
+  user:User = new User();
+  constructor(http:HttpClient) { 
+    
+  }
 
   ngOnInit() {
   }
 
+  handleSignUp(){
+    console.log(this.user.firstname);
+  }
+}
+
+export class User{
+  firstname;
+  lastname;
+  email;
+  phone;
+  country;
 }
