84c9c649858c99e6678e642f86043920a0417028
Nitin <nitin_saxena@live.in>
2019-07-11 11:50:23 +0530

    update

--- src/app/currencyconverter.pipe.ts
+++ src/app/currencyconverter.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CurrencyconverterPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return value+ " USD";
+    let reqCurrency = "INR"
+    if(args[0]!=null){
+      reqCurrency = args[0];
+    }
+    return (value*68.34)+ reqCurrency;
   }
 
 }
